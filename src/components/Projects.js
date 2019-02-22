1de6439f0230d7ed51c7ac748e61f4b075f10ca5
mohammed-adil <adilsheik98@gmail.com>
2019-02-22 08:55:21 +0530

    Update project description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,21 +15,21 @@ const projectData = {
           "id": 2,
           "title": "glug-pace.github.io",
           "link": `https://github.com/mohammed-adil/glug-pace.github.io`,
-          "description": "A travell guider android application targeted for a local area, using PHP for backend to retrieve data from database.",
+          "description": "Official website for GLUG PACE which is a group of people who works for Free Software Movement Karnataka.",
           "tags": ['HTML', 'CSS', 'blog', 'static-site']
         },
         {
           "id": 3,
           "title": "Portal",
           "link": `https://github.com/mohammed-adil/portal`,
-          "description": "A travell guider android application targeted for a local area, using PHP for backend to retrieve data from database.",
+          "description": "A simple blog application, developed using flask (a python library for used developing web applications).",
           "tags": ['Python', 'Flask', 'MySQLi']
         },
       {
         "id": 4,
         "title": "react-todo",
         "link": `https://github.com/mohammed-adil/react-todo`,
-        "description": "A travell guider android application targeted for a local area, using PHP for backend to retrieve data from database.",
+        "description": "A plain and simple practical representation of CRUD app done using REACT JS(JavaScript library).",
         "tags": ['React.js', 'todo','CRUD']
       }
   ]
